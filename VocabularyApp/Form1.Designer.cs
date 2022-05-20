89774fb171dab7e4a11f2d8aa25e31f34bbf03d2
Ömer Korkmaz <93475752+omeredk@users.noreply.github.com>
2022-05-20 23:47:26 +0300

    Add project files.

---
+++ VocabularyApp/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace VocabularyApp
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.textBoxWord = new System.Windows.Forms.TextBox();
+            this.lblWord = new System.Windows.Forms.Label();
+            this.richTextBoxMeaning = new System.Windows.Forms.RichTextBox();
+            this.lblMeaning = new System.Windows.Forms.Label();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxWord
+            // 
+            this.textBoxWord.Location = new System.Drawing.Point(132, 36);
+            this.textBoxWord.Name = "textBoxWord";
+            this.textBoxWord.Size = new System.Drawing.Size(100, 20);
+            this.textBoxWord.TabIndex = 0;
+            this.textBoxWord.Tag = "";
+            // 
+            // lblWord
+            // 
+            this.lblWord.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lblWord.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblWord.Location = new System.Drawing.Point(46, 36);
+            this.lblWord.Name = "lblWord";
+            this.lblWord.Size = new System.Drawing.Size(75, 20);
+            this.lblWord.TabIndex = 1;
+            this.lblWord.Text = "Word:";
+            this.lblWord.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // richTextBoxMeaning
+            // 
+            this.richTextBoxMeaning.Location = new System.Drawing.Point(132, 74);
+            this.richTextBoxMeaning.Name = "richTextBoxMeaning";
+            this.richTextBoxMeaning.Size = new System.Drawing.Size(100, 55);
+            this.richTextBoxMeaning.TabIndex = 2;
+            this.richTextBoxMeaning.Text = "";
+            // 
+            // lblMeaning
+            // 
+            this.lblMeaning.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lblMeaning.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblMeaning.Location = new System.Drawing.Point(46, 74);
+            this.lblMeaning.Name = "lblMeaning";
+            this.lblMeaning.Size = new System.Drawing.Size(75, 20);
+            this.lblMeaning.TabIndex = 3;
+            this.lblMeaning.Text = "Translation:";
+            this.lblMeaning.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(243, 34);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 25);
+            this.btnSearch.TabIndex = 4;
+            this.btnSearch.Text = "Find";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(243, 74);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 25);
+            this.btnAdd.TabIndex = 5;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(243, 104);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 25);
+            this.btnDelete.TabIndex = 7;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(362, 175);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.lblMeaning);
+            this.Controls.Add(this.richTextBoxMeaning);
+            this.Controls.Add(this.lblWord);
+            this.Controls.Add(this.textBoxWord);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "frmMain";
+            this.Text = "Vocabulary App - @omeredk";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxWord;
+        private System.Windows.Forms.Label lblWord;
+        private System.Windows.Forms.RichTextBox richTextBoxMeaning;
+        private System.Windows.Forms.Label lblMeaning;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnDelete;
+    }
+}
+
---
+++ VocabularyApp/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace VocabularyApp
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+        
+        //YOU SHOULD ENTER YOUR CONNECTION STRING FIRST
+        SqlConnection connection = new SqlConnection("ENTER YOUR CONNECTION STRING HERE");
+
+        //FIND FUNCTION STARTS HERE
+        void listWord()
+        {
+            connection.Open();
+            SqlCommand cmdList = new SqlCommand("SELECT translation FROM words WHERE word='"+textBoxWord.Text+"'", connection);
+            SqlDataReader dr = cmdList.ExecuteReader();
+            if (dr.Read())
+            {
+                richTextBoxMeaning.Text = dr["translation"].ToString();
+                dr.Close();
+            }
+            else
+            {
+                MessageBox.Show("Word Not Found", "ERROR",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                dr.Close();
+            }
+            connection.Close();
+        }
+
+        //ADD FUNCTION STARTS HERE
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            connection.Open();
+            SqlCommand cmdCheck = new SqlCommand("SELECT * FROM words WHERE word='" + textBoxWord.Text + "'", connection);
+            SqlDataReader dr = cmdCheck.ExecuteReader();
+            if (dr.Read())
+            {
+                MessageBox.Show("entered word is already exists! ", "WARNING");
+                dr.Close();
+            }
+
+            else
+            {
+                SqlCommand cmd = new SqlCommand("INSERT INTO words(word, translation) VALUES(@1,@2)", connection);
+                cmd.Parameters.AddWithValue("@1", textBoxWord.Text);
+                cmd.Parameters.AddWithValue("@2", richTextBoxMeaning.Text);
+                dr.Close();
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("SUCCES!");
+            }
+                connection.Close();
+            textBoxWord.Text = "";
+            richTextBoxMeaning.Text = "";
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            listWord();
+        }
+
+        //DELETE FUNCTION STARTS HERE
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            DialogResult answer;
+            answer = MessageBox.Show("Are you sure you want to delete this?", "WARNING!", MessageBoxButtons.YesNo, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button2);
+            if (answer == DialogResult.Yes)
+            {
+                connection.Open();
+                SqlCommand cmdDelete = new SqlCommand("DELETE FROM words WHERE word='" + textBoxWord.Text + "'", connection);
+                cmdDelete.ExecuteNonQuery();
+                connection.Close();
+                MessageBox.Show("Word has been deleted", "Succes!");
+            }
+            textBoxWord.Text = "";
+            richTextBoxMeaning.Text = "";
+        }
+    }
+}
